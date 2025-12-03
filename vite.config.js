05e369db8e814ea51705b6397b91533b61ca12c4
linhongtao <lht0812@hotmail.com>
2025-12-03 15:35:24 +0800

    优化

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/https://github.com/LIN-HongTao/AI-cal-var/",   // 关键：GitHub Pages 用
 })
